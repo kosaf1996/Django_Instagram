9dc9b6ff5b9fcb3a8548809517c34b2f1643e6a0
kosaf1996 <kosaf1996@naver.com>
2022-03-03 21:47:54 +0900

    commit

--- content/views.py
+++ content/views.py
@@ -1,17 +1,41 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from instagram.settings import MEDIA_ROOT
 from .models import Feed
+
 
 class Main(APIView):
     def get(self, request):
         # Feed 모든 데이터를 가져옴 -> select * from content_feed -> feed 를 최신부터 보여주기위해 order_by 사용
         feed_list = Feed.objects.all().order_by('-id')
 
-        for feed in feed_list:
-            pass
         return render(request, "instagram/main.html", context=dict(feed=feed_list))
 
+
+class UploadFeed(APIView):
     def post(self, request):
-        return render(request, "instagram/main.html")
+
+        #파일 저장 기능
+        file = request.FILES['file']
+        uuid_name = uuid4().hex #이미지가 특수문자등 난해 할수 있어 uuid 로 생성된 이름 사용
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        #main.html의 AJAX 로 부터 데이터 받기
+        image = uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image,content=content,user_id=user_id,profile_image=profile_image,like_count=0)
+
+        return Response(status=200)
--- instagram/urls.py
+++ instagram/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from content.views import Main
+from content.views import Main, UploadFeed
+from instagram import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Main.as_view()),
+    path('main/', Main.as_view()),
+    path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
