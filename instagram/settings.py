8a403b1dd5fb6a69ce41ba0393ee8578904e2705
Gyoung Min <73932913+kosaf1996@users.noreply.github.com>
2022-07-17 23:44:25 +0900

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5kfsyafmscfcg_0^8jsr&7*6hf_!o)3!-pan!4u(4iy6gz047-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
