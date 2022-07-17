b2afca49ad8ae20e4e0e03dc23d42b2806203d29
Gyoung Min <73932913+kosaf1996@users.noreply.github.com>
2022-07-17 22:21:11 +0900

    Update urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from content.views import Main
 from instagram import settings
+from user.views import Login
 
 urlpatterns = [
+    path('', Login.as_view()),
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
     path('content/', include('content.urls')),
